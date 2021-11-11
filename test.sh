0040568d76959b3a51f3757811fda8d8e7d7908c
Francois Berenger <unixjunkie@sdf.org>
2021-11-11 16:20:05 +0900

    also test the RBF kernel

--- test.sh
+++ test.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+set -x # DEBUG
+set -u # strict
+
 make
 
-# train a linear-SVR; C=0.005 is close to optimal for this dataset
-./svmwrap -q -np 5 --NxCV 5 -c 0.005 --pairs -i data/CHEMBL2842_std.AP
+# train a SVR with a linear kernel; C=0.005 is close to optimal for this dataset; 5xCV
+./svmwrap -q --pairs -np 5 --NxCV 5 -c 0.005  --feats 5183 -i data/CHEMBL2842_std.AP
+
+# train a SVR with RBF kernel (default e, good C and gamma are known); 5xCV
+./svmwrap -q --pairs -np 5 --NxCV 5 -c 10 -g 0.0005 --kernel RBF --feats 5183 \
+          -i data/CHEMBL2842_std.AP
