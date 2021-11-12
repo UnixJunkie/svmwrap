863801a8cd1159e3ace06e424f02c5569e6322e7
Francois Berenger <unixjunkie@sdf.org>
2021-11-12 16:15:58 +0900

    test.sh: cut long lines

--- test.sh
+++ test.sh
@@ -7,10 +7,13 @@ NPROCS=`getconf _NPROCESSORS_ONLN`
 
 make
 
-# train a SVR with a linear kernel; C=0.005 is close to optimal for this dataset; 5xCV
-./svmwrap -q --pairs -np ${NPROCS} --NxCV 5 -c 0.005  --feats 5183 -i data/CHEMBL2842_std.AP
+# train a SVR with a linear kernel;
+# C=0.005 is close to optimal for this dataset; 5xCV
+./svmwrap -q --pairs -np ${NPROCS} --NxCV 5 -c 0.005 --feats 5183 \
+          -i data/CHEMBL2842_std.AP
 
-# train a SVR with RBF kernel (default e, good C and gamma are known); 5xCV
+# train a SVR with RBF kernel
+# (default e, good C and gamma are known); 5xCV
 ./svmwrap -q --pairs -np ${NPROCS} --NxCV 5 -c 10 -g 0.0005 --kernel RBF --feats 5183 \
           -i data/CHEMBL2842_std.AP
 
@@ -18,3 +21,8 @@ make
 ./svmwrap -q --pairs -np ${NPROCS} --NxCV 5 \
           -c 50 -g '2e-05' -r 0.0 --kernel Sig \
           --feats 5183 -i data/CHEMBL2842_std.AP
+
+# # train a SVR with the polynomial kernel
+# ./svmwrap -q --pairs -np ${NPROCS} --NxCV 5 \
+#           --scan-c --scan-g --kernel Pol \
+#           --feats 5183 -i data/CHEMBL2842_std.AP
